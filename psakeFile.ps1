438fa78543a0b2efe07953dd826dc838b44996f1
Gilbert Sanchez <me@gilbertsanchez.com>
2023-09-02 22:50:17 +0000

    Update to compiled module

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -1,6 +1,6 @@
 Properties {
     # Set this to $true to create a module with a monolithic PSM1
-    $PSBPreference.Build.CompileModule = $false
+    $PSBPreference.Build.CompileModule = $true
     $PSBPreference.Help.DefaultLocale = 'en-US'
     $PSBPreference.Test.OutputFile = 'out/testResults.xml'
     $PSBPreference.Test.OutputFormat = 'JUnitXml'
